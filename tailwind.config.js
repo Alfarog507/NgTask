5ef59b55a55895dc8000a45aac0ddb42cddfd7e3
Alfarog507 <alfarog507@gmail.com>
2025-02-18 22:01:56 -0600

    Comprobamos la configuracion de tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// filepath: /C:/Users/alfar/Escritorio/Portafolio/ng-task/tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts,scss}", // Ajusta esto según la estructura de tu proyecto
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
