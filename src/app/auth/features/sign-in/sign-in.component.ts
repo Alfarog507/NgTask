ebdb8119d784d3c41428e2d54b500255c096c4b8
Alfarog507 <alfarog507@gmail.com>
2025-02-23 22:27:11 -0600

    Creamos componente sign-in y sign-up + route

---
+++ src/app/auth/features/sign-in/sign-in.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-in',
+  imports: [],
+  templateUrl: './sign-in.component.html',
+  styles: ``,
+})
+export default class SignInComponent {}
---
+++ src/app/auth/features/sign-up/sign-up.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up',
+  imports: [],
+  templateUrl: './sign-up.component.html',
+  styles: ``,
+})
+export default class SignUpComponent {}
