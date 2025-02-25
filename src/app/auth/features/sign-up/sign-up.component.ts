d2073f4cd6623cb57e513a1df716626113b65747
Alfarog507 <alfarog507@gmail.com>
2025-02-24 22:43:02 -0600

    Creamos interface sign-up

--- src/app/auth/features/sign-up/sign-up.component.ts
+++ src/app/auth/features/sign-up/sign-up.component.ts
@@ -1,9 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+interface SignUpForm {
+  email: FormControl<string | null>;
+  password: FormControl<string | null>;
+  confirmPassword: FormControl<string | null>;
+  terms: FormControl<boolean | null>;
+}
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
   styles: ``,
 })
-export default class SignUpComponent {}
+export default class SignUpComponent {
+  private _formBuilder = inject(NonNullableFormBuilder);
+
+  signUpForm = this._formBuilder.group<SignUpForm>({
+    email: this._formBuilder.control('', [
+      Validators.required,
+      Validators.email,
+    ]),
+    password: this._formBuilder.control('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+    confirmPassword: this._formBuilder.control('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+    terms: this._formBuilder.control(false, Validators.requiredTrue),
+  });
+
+  onSubmit() {
+    if (this.signUpForm.invalid) return;
+
+    const { email, password, confirmPassword, terms } = this.signUpForm.value;
+  }
+}
