b7ccc3f309bf665358a85fb9947ecd0aefeb22c8
Alfarog507 <alfarog507@gmail.com>
2025-02-25 22:55:31 -0600

    Agregamos el componente task-list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,12 @@ export const routes: Routes = [
     path: 'auth',
     loadChildren: () => import('./auth/features/auth.routes'),
   },
+  {
+    path: 'task',
+    loadChildren: () => import('./task/features/task.routes'),
+  },
+  {
+    path: '**',
+    redirectTo: 'auth/sign-up',
+  },
 ];
