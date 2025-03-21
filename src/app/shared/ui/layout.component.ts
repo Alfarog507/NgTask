f4dda7ce65881ab5cf16796b5643c16663f61b04
Alfarog507 <alfarog507@gmail.com>
2025-03-20 22:48:06 -0600

    Cambiamos nombre de app

--- src/app/shared/ui/layout.component.ts
+++ src/app/shared/ui/layout.component.ts
@@ -11,7 +11,7 @@ import { NgxSonnerToaster } from 'ngx-sonner';
     <header class="h-[80px] mb-8 w-full max-w-screen-lg mx-auto px-4">
       <nav class="flex items-center justify-between h-full">
         <a class="text-2xl font-bold cursor-pointer" routerLink="/task"
-          >Ng Task</a
+          >Task CRUD</a
         >
         <button
           class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 cursor-pointer"
